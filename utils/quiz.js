a08dfa2e5b3c85a9339df014f8ef4674adb5e519
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2023-05-08 14:34:30 +0200

    progress

--- utils/quiz.js
+++ utils/quiz.js
@@ -6,6 +6,7 @@ export default function quiz(questions) {
   let currentQuestionIndex = 0;
   const lastQuestionIndex = questions.length - 1;
   let selectedAlternative = null;
+  let score = 0;
 
   const questionProgress = document.createElement("p");
   questionProgress.className = "quiz__progress";
@@ -25,8 +26,12 @@ export default function quiz(questions) {
   renderCurrentQuestion();
 
   // Handlers
+  function setAlternative(index) {
+    selectedAlternative = index;
+  }
+
   function renderCurrentQuestion() {
-    questionProgress.innerHTML = `${currentQuestionIndex + 1}/${
+    questionProgress.innerHTML = `${currentQuestionIndex + 1} / ${
       questions.length
     }`;
 
@@ -48,6 +53,9 @@ export default function quiz(questions) {
     for (let i = 0; i < shuffledAlternatives.length; i++) {
       const { text } = shuffledAlternatives[i];
 
+      const alternativeContainer = document.createElement("div");
+      alternativeContainer.className = "quiz__alternative-container";
+
       const radioButton = document.createElement("input");
       radioButton.className = "quiz__alternative";
       radioButton.id = `alternative${i + 1}`;
@@ -63,19 +71,50 @@ export default function quiz(questions) {
       alternativeLabel.htmlFor = `alternative${i + 1}`;
       alternativeLabel.innerHTML = text;
 
-      alternativesContainer.appendChild(radioButton);
-      alternativesContainer.appendChild(alternativeLabel);
+      alternativeContainer.appendChild(radioButton);
+      alternativeContainer.appendChild(alternativeLabel);
+
+      alternativesContainer.appendChild(alternativeContainer);
     }
 
     selectedAlternative = null;
 
     questionContainer.appendChild(questionText);
     questionContainer.appendChild(alternativesContainer);
+  }
 
-    function setAlternative(index) {
-      selectedAlternative = index;
+  function handleSubmit() {
+    if (selectedAlternative === null) return;
+
+    if (currentQuestionIndex === lastQuestionIndex) {
+      let resultMessage;
+
+      if (score <= 6) {
+        resultMessage =
+          "Dessverre, det virker ikke som om programmeringsklubben vår passer for deg akkurat nå. Men ikke gi opp, og fortsett å utforske interesser du har!";
+      } else if (score <= 12) {
+        resultMessage =
+          "Du har noen grunnleggende ferdigheter og interesser innen programmering, og vi tror du kan ha nytte og glede av å være med i klubben vår. Velkommen om bord!";
+      } else {
+        resultMessage =
+          "Det er tydelig at du har en sterk interesse for programmering og teknologi. Vi tror at vår klubben kan gi deg den ekstra fordelen du trenger for å ta dine ferdigheter til neste nivå. Vi ser frem til å ha deg med på laget!";
+      }
+
+      quizContainer.innerHTML = `
+        <p class="quiz__result-message">${resultMessage}</p>
+      `;
+
+      return;
     }
+
+    score +=
+      questions[currentQuestionIndex].alternatives[selectedAlternative].points;
+    console.log(score);
+
+    currentQuestionIndex++;
+    renderCurrentQuestion();
   }
 
   // Events
+  submitButton.addEventListener("click", handleSubmit);
 }
