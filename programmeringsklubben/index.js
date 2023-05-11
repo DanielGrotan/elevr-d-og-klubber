6f243db463c57836e46ac4b151117187a04587aa
Daniel Grøtan Gregusson <danielgrotangregusson@gmail.com>
2023-05-11 16:36:57 +0200

    changes

--- programmeringsklubben/index.js
+++ programmeringsklubben/index.js
@@ -1,8 +1,6 @@
 import clubMembersList from "../assets/data/club-members.json" assert { type: "json" };
 import clubProjectsJson from "../assets/data/club-projects.json" assert { type: "json" };
 import questions from "../assets/data/club-quiz.json" assert { type: "json" };
-import imagesJson from "../assets/data/news-images.json" assert { type: "json" };
-import carousel from "../utils/carousel.js";
 import clubMembers from "../utils/clubMembers.js";
 import clubProjects from "../utils/clubProjects.js";
 import colorTheme from "../utils/colorTheme.js";
@@ -12,7 +10,6 @@ import quiz from "../utils/quiz.js";
 
 colorTheme();
 mobileNav();
-carousel(imagesJson);
 clubMembers(clubMembersList);
 clubProjects(clubProjectsJson);
 await dailyJoke();
